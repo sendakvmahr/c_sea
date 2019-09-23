d630ab1513e9d11607a938b3a03eb0b8e237eec6
sendak <vivianmahr@gmail.com>
2019-09-22 22:38:19 -0700

    Inefficient document recorder done

---
+++ text/one-shots/db_wrapper.py
@@ -0,0 +1,83 @@
+"""A lot of one shots actually make their own dbs"""
+import sqlite3
+import os.path
+from text import token
+DEFAULT_DB = "text.db"
+
+class DB_Connection():
+    def __init__(self, token_converter, threshold=100, db=DEFAULT_DB):
+        self.commands = {
+            "add": "INSERT INTO VOCAB (ID, WORD, WORD_COUNT) VALUES (null, ?, ?)",
+            "increment": "UPDATE VOCAB SET WORD_COUNT = WORD_COUNT + ? WHERE WORD=?"
+        }
+        self.queue = {
+            "add" : [],
+            "increment" : []
+        }
+        makeDB = not os.path.exists(db)
+        self.db_conn = sqlite3.connect(db)
+        self._cursor = self.db_conn.cursor()
+        if makeDB:
+            self._cursor.execute("CREATE TABLE `VOCAB` ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, `WORD` TEXT, `WORD_COUNT` INTEGER )")
+        self.threshold = threshold
+        self.token_converter = token_converter
+
+    def increment(self, word):
+        is_add = not self.is_in(word)
+        if (not is_add):
+            for key, items in self.queue.items():
+                if word in items:
+                    pass 
+#modify the item so increment is bigger, but should reform command queues first
+        #check to see if word is already added, in that case increment it in queue command too
+        command = "add" if is_add else "increment"
+        inputs = (word, 1) if is_add else (1, word)
+        self._queue_command(command, inputs)
+    
+    def is_in(self, word):
+        self._run_command('SELECT * FROM VOCAB WHERE WORD=?', word)
+        return bool(self._cursor.fetchone())
+    
+    def _run_command(self, command, word):
+        # convert word to db using token_to_db
+        if isinstance(word, token.Token):
+            word = word.text
+        self._cursor.execute(command, (word,))
+        self.db_conn.commit()
+
+    def _run_many_commands(self, command, inputs):
+        clean_inputs = []
+        for i in inputs:
+            to_append = []
+            for objs in i:
+                to_append.append(objs.text if isinstance(objs, token.Token) else objs)
+            clean_inputs.append(tuple(to_append))
+        self._cursor.executemany(command, clean_inputs)
+        self.db_conn.commit()
+        
+    def _queue_command(self, command, word):
+        self.queue[command].append(word);
+        self._check_queue()
+    
+    def _check_queue(self):
+        sum = 0
+        for key, item in self.queue.items():
+            sum += len(item)
+        if sum >= self.threshold:
+            self._push_queue()
+
+    def _push_queue(self):
+        for command in self.queue.keys():
+            self._run_many_commands(self.commands[command], self.queue[command])
+        self.db_conn.commit()
+        self.queue = {
+            "add" : [],
+            "increment" : []
+        }
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._push_queue()
+        self.db_conn.exit()
+    
+
+    # on close commit and close
--- text/one-shots/wiki_reader.py
+++ text/one-shots/wiki_reader.py
@@ -7,9 +7,18 @@ DEFAULT_DB = "wikiInhaler.db"
 WIKI_DIR = "../corpora/wikiextractor/"
 DEG_ASSOCIATION = 2
 SPACY_EN = spacy.load("en") 
-
+DB = sqlite3.connect("text.db")
+CURSOR = DB.cursor()
+make = True
+make_commands = [ 
+"""CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word` TEXT NOT NULL, `count` INTEGER NOT NULL )""", 
+"""CREATE TABLE "pairings" ( `word1id` INTEGER NOT NULL, `word2id` INTEGER NOT NULL, `count` INTEGER, PRIMARY KEY(`word2id`,`word1id`) )"""
+]
 
 def read_data(file_name):
+	if make:
+		for c in make_commands:
+			CURSOR.execute(c)
 	lines = []
 	with open(file_name) as file:
 		lines = file.readlines()
@@ -19,12 +28,53 @@ def read_data(file_name):
 		#if counter >= 5:
 		#	return
 		#print("---")
-		print(extract_data(line))
+		print(line)
+		pairings, frequencies, pos = extract_data(line.replace('"', ""))
+		commands = []
+		# MERGE BETWEEN LINES AND ONLY SHIP TO DB WHEN DONE PER FILE
+		if pairings != 0:
+			for word, count in frequencies.items():
+				if is_in_word(word):
+					commands.append(["UPDATE words SET count = count + ? WHERE word=?", (count, word)])
+				else:
+					commands.append(["INSERT INTO words (id, word, count) VALUES (null, ?, ?)", (word, count)])
+			for c in commands:
+				try:
+					CURSOR.execute(c[0], c[1])
+				except: 
+					print(c)
+					raise TypeError
+			DB.commit()
+			commands = []
+			for pairing, count in pairings.items():
+				ids = get_ids(pairing)
+				if is_in_pairing(ids):
+					commands.append(["INSERT INTO pairings (word1id, word2id, count) VALUES (?, ?, ?)", (ids[0], ids[1], count)])
+				else:
+					commands.append(["UPDATE pairings SET count = count + ? WHERE word1id=? AND word2id=?", (count, ids[0], ids[1])])
+			for c in commands:
+				try:
+					CURSOR.execute(c[0], c[1])
+				except: 
+					print(c)
+					raise TypeError
 
-	# import db connection and send info here when done
+
+
+def get_ids(pair):
+	id0 = CURSOR.execute('SELECT * FROM words WHERE word="{}"'.format(pair[0])).fetchone()[0]
+	id1 = CURSOR.execute('SELECT * FROM words WHERE word="{}"'.format(pair[1])).fetchone()[0]
+	return (id0, id1)
+
+def is_in_pairing(ids):
+    CURSOR.execute('SELECT * FROM pairings WHERE word1id=? AND word2id=?', (ids[0], ids[1]))
+
+def is_in_word(word):
+    CURSOR.execute('SELECT * FROM words WHERE word=?', (word,))
+    return bool(CURSOR.fetchone())
 
 def extract_data(line):
-	if line == "" or line[0] == "<": return
+	if line == "" or line[0] == "<": return [0,0,0]
 	pairings = defaultdict(int)
 	frequencies = defaultdict(int)
 	words_pos = defaultdict(int)
