8bbeee93d305327d04b4d91a93be5165b6c0ffad
sendak <vivianmahr@gmail.com>
2019-09-23 23:02:24 -0700

    hopefully a scrappy db

--- text/one-shots/wiki_reader.py
+++ text/one-shots/wiki_reader.py
@@ -2,6 +2,7 @@ import os
 import spacy
 import sqlite3
 from collections import defaultdict
+import re
 
 DEFAULT_DB = "wikiInhaler.db"
 WIKI_DIR = "../corpora/wikiextractor/"
@@ -9,69 +10,77 @@ DEG_ASSOCIATION = 2
 SPACY_EN = spacy.load("en") 
 DB = sqlite3.connect("text.db")
 CURSOR = DB.cursor()
+read = 0
+total = 109579737
 make = True
 make_commands = [ 
 """CREATE TABLE `words` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `word` TEXT NOT NULL, `count` INTEGER NOT NULL )""", 
-"""CREATE TABLE "pairings" ( `word1id` INTEGER NOT NULL, `word2id` INTEGER NOT NULL, `count` INTEGER, PRIMARY KEY(`word2id`,`word1id`) )"""
+"""CREATE TABLE "pairings" ( `word1id` INTEGER NOT NULL, `word2id` INTEGER NOT NULL, `count` INTEGER, PRIMARY KEY(`word1id`,`word2id`) )"""
 ]
 
 def read_data(file_name):
 	if make:
 		for c in make_commands:
 			CURSOR.execute(c)
+	update_word = "UPDATE words SET count = count + ? WHERE word=?"
+	insert_word = "INSERT INTO words (id, word, count) VALUES (null, ?, ?)"
+	update_pairings = "UPDATE pairings SET count = count + ? WHERE word1id=? AND word2id=?"
+	insert_pairings = "INSERT INTO pairings (word1id, word2id, count) VALUES (?, ?, ?)"
+
+	c = defaultdict(list)
+
+	global read 			
 	lines = []
 	with open(file_name) as file:
 		lines = file.readlines()
-	#counter = 0
 	for line in lines:
-		#counter+=1
-		#if counter >= 5:
-		#	return
-		#print("---")
-		print(line)
+		c = defaultdict(list)
+		read += 1
+		print(read/total)
 		pairings, frequencies, pos = extract_data(line.replace('"', ""))
-		commands = []
 		# MERGE BETWEEN LINES AND ONLY SHIP TO DB WHEN DONE PER FILE
 		if pairings != 0:
 			for word, count in frequencies.items():
 				if is_in_word(word):
-					commands.append(["UPDATE words SET count = count + ? WHERE word=?", (count, word)])
+					c[update_word].append((count, word))
 				else:
-					commands.append(["INSERT INTO words (id, word, count) VALUES (null, ?, ?)", (word, count)])
-			for c in commands:
-				try:
-					CURSOR.execute(c[0], c[1])
-				except: 
-					print(c)
-					raise TypeError
+					c[insert_word].append((word, count))
+			CURSOR.executemany(insert_word, c[insert_word])
+			CURSOR.executemany(update_word, c[update_word])
 			DB.commit()
-			commands = []
 			for pairing, count in pairings.items():
 				ids = get_ids(pairing)
 				if is_in_pairing(ids):
-					commands.append(["INSERT INTO pairings (word1id, word2id, count) VALUES (?, ?, ?)", (ids[0], ids[1], count)])
+					c[update_pairings].append((count, ids[0], ids[1]))
 				else:
-					commands.append(["UPDATE pairings SET count = count + ? WHERE word1id=? AND word2id=?", (count, ids[0], ids[1])])
-			for c in commands:
-				try:
-					CURSOR.execute(c[0], c[1])
-				except: 
-					print(c)
-					raise TypeError
+					c[insert_pairings].append((ids[0], ids[1], count))
+			CURSOR.executemany(update_pairings, c[update_pairings])
+			CURSOR.executemany(insert_pairings, c[insert_pairings])
+			DB.commit()
 
 
 
 def get_ids(pair):
-	id0 = CURSOR.execute('SELECT * FROM words WHERE word="{}"'.format(pair[0])).fetchone()[0]
-	id1 = CURSOR.execute('SELECT * FROM words WHERE word="{}"'.format(pair[1])).fetchone()[0]
+	print(pair)
+	id0 = CURSOR.execute('SELECT * FROM words WHERE word=?', (pair[0],)).fetchone()[0]
+	id1 = CURSOR.execute('SELECT * FROM words WHERE word=?', (pair[1],)).fetchone()[0]
 	return (id0, id1)
 
 def is_in_pairing(ids):
     CURSOR.execute('SELECT * FROM pairings WHERE word1id=? AND word2id=?', (ids[0], ids[1]))
+    return bool(CURSOR.fetchone())
 
 def is_in_word(word):
     CURSOR.execute('SELECT * FROM words WHERE word=?', (word,))
     return bool(CURSOR.fetchone())
+
+def is_english(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return re.match(r".*[a-zA-Z]", s)
 
 def extract_data(line):
 	if line == "" or line[0] == "<": return [0,0,0]
@@ -80,28 +89,38 @@ def extract_data(line):
 	words_pos = defaultdict(int)
 	line = SPACY_EN(line)
 	for token in line: 
-		if token.pos_ != "PROPN":
+		if token.pos_ != "PROPN" and is_english(token.text):
 			words_pos[token.text.lower()] += 1
 			frequencies[token.text.lower()] += 1
 	cap = len(line)
 	for ti in range(cap):
-		if line[ti].pos_ != "PROPN":
+		if line[ti].pos_ != "PROPN" and is_english(token.text):
 			lower = ti if ti < DEG_ASSOCIATION else DEG_ASSOCIATION
 			upper = cap - ti -1 if ti >= cap - DEG_ASSOCIATION -1 else DEG_ASSOCIATION
 			# the -1 is because (range(1, 1) won't run once for 1
 			for i in range(lower):
-				if line[ti-i-1].pos_ != "PROPN":
+				if line[ti-i-1].pos_ != "PROPN" and is_english(token.text):
 					pairings[(line[ti-i-1].text.lower(), line[ti].text.lower())] += 1
 			for i in range(upper):
-				if line[ti+i+1].pos_ != "PROPN":
+				if line[ti+i+1].pos_ != "PROPN" and is_english(token.text):
 					pairings[(line[ti].text.lower(), line[ti+i+1].text.lower())] += 1 			
 	return [pairings, frequencies, words_pos]
 
 
 if __name__ == "__main__":
-	test = "AA/wiki_00"
-	read_data(WIKI_DIR + test)
-	"""
-	for f in os.listdir(WIKI_DIR):
-	read_data(f)
-	"""
+	#test = "AA/wiki_00"
+	#read_data(WIKI_DIR + test)
+	abspath = os.path.abspath(WIKI_DIR)
+	lines = 0
+	for f1 in os.listdir(WIKI_DIR):
+		f1path = os.path.join(abspath, f1)
+		if (os.path.isdir(f1path)):
+			for f in os.listdir(os.path.join(abspath, f1)):
+				fpath = os.path.join(f1path, f)
+				if not (os.path.isdir(fpath)):
+					read_data(fpath)
+					#file = open(os.path.join(abspath, f1, f), errors='ignore')
+					#lines += len(file.readlines())
+					#file.close()
+	#print(lines)
+	#109579737 lines
